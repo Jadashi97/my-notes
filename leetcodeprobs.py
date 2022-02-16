e438fbe7c10b581435f136840c0c5c4ee6b8a8c9
jadashi97 <njadauniversal23@yahoo.com>
2022-02-16 11:44:58 -0600

    first commit

---
+++ leetcodeprobs.py
@@ -0,0 +1,10 @@
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,5,6]
+m = 3
+n = 3
+class solutions(object):
+    def merge(self,num1, nums2, m, n):
+        for i in range(object):
+            self.append([num1])
+
+
---
+++ saylortuts.py
@@ -0,0 +1,4 @@
+name = ['lokose']
+print(name)
+
+print("hello world")
