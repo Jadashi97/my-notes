d07397fcd82df1998053b271796b4203524278cd
jadashi97 <njadauniversal23@yahoo.com>
2022-02-17 15:04:11 -0600

    worked on more probs

--- leetcodeprobs.py
+++ leetcodeprobs.py
@@ -1,5 +1,27 @@
+
+#HW1
 nums = [2,7,11,15]
 target = 9
-class solution(sum):
+class solution(object):
     def twosum(self, nums, target):
-        for 
+        nums_indx = { }
+        for i in range(0, len(nums)):
+            diff = target - nums[i]
+            if diff in nums_indx:
+                return [i, nums_indx[diff]]
+            nums_indx[nums[1]] = i
+
+#data structures. Q1
+nums = [2,3,1,2,3]
+#class Solution(object):
+def containsDuplicate(nums):
+    S = { }
+    for i in nums:
+        if i in S:
+            return True
+        else:
+            S[i] = 1
+    return False
+
+print(containsDuplicate(nums))
+
