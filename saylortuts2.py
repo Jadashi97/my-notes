fe1a506ef2f608905d8a6aa32c02762c5c087e0a
jadashi97 <njadauniversal23@yahoo.com>
2022-02-24 11:18:09 -0600

    feat:(docs):worked on more regular expression problems

--- saylortuts2.py
+++ saylortuts2.py
@@ -15,10 +15,32 @@ compiled_pattern = re.compile(pattern)
 for person in personList:
     if compiled_pattern.match(person):
         print(person)
-'''
-#2
+
+#5 & 6
 list = ['adfac', 'babba', 'cbaca', ' fbaceeef']
 pattern = 'a.*b'
 for i in list:
-    if re.search(pattern, list):
+    if re.match(pattern, i):
         print(i)
+
+def re_pattern(text, pattern):
+    print("  '{}'".format(text))
+    for match in re.finditer(pattern, text):
+        s = match.start()
+        e = match.end()
+        substr_match = text[s:e]
+        dot_prefix = '.'* s
+        print("  {}'{}'".format(dot_prefix, substr_match))
+    print()
+    return
+
+#7 re_pattern('Michael is in Los Angeles with his family', '[A-Z][a-z]+')
+#8 re_pattern('cddccdddc', 'cd?')
+#9 re_pattern('cdcddccddddc', 'cd{2,3}')
+
+#10
+f = open(' str_data.txt', 'r')
+for str_match in re.finditer('a((a+)|(b+))', f.read()):
+    print(str_match.start(), str_match.end())
+f.close()
+'''
